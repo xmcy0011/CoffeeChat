7fd9b6efc35b9df5a7c51aae9703ffdbd1a2d621
xuyc <xmcy0011@sina.com>
2019-09-25 10:38:49 +0800

    heartbeat

--- server/src/app/demo/tcp_client.go
+++ server/src/app/demo/tcp_client.go
@@ -42,6 +42,10 @@ func main() {
 		logger.Sugar.Fatal("login error:", err.Error())
 	} else {
 		logger.Sugar.Info("login success")
+
+		heartBeatTicker = time.NewTicker(30 * time.Second)
+		go heartBeat()
+		go read()
 	}
 
 	for {
@@ -54,7 +58,7 @@ func main() {
 			text := scanner.Text()
 			switch text {
 			case "1":
-				showRecentSessionList()
+				reqRecentSessionList()
 				break
 			default:
 				logger.Sugar.Info("unknown command,please try again...")
@@ -82,10 +86,14 @@ func login() error {
 	_ = send(uint16(cim.CIMCmdID_kCIM_CID_LOGIN_AUTH_LOGOUT_REQ), req)
 
 	res := &cim.CIMAuthTokenRsp{}
-	_, dataBuff, err := read()
+
+	buffLen, err := tcpConn.Read(tcpBuffer)
 	if err != nil {
 		return err
 	}
+
+	dataBuff := tcpBuffer[cim.IMHeaderLen:buffLen] // body=len-headLen
+
 	err = proto.Unmarshal(dataBuff, res)
 	if err != nil {
 		return err
@@ -96,9 +104,6 @@ func login() error {
 	}
 
 	//_ = tcpConn.SetReadDeadline(time.Unix(0, 0))
-	heartBeatTicker = time.NewTicker(30 * time.Second)
-	go heartBeat()
-
 	return nil
 }
 
@@ -114,33 +119,41 @@ func send(cimId uint16, message proto.Message) error {
 	return nil
 }
 
-func read() (*cim.ImHeader, []byte, error) {
-	buffLen, err := tcpConn.Read(tcpBuffer)
-	if err != nil {
-		return nil, nil, err
-	}
-	header := &cim.ImHeader{}
-	header.ReadHeader(tcpBuffer, buffLen)
-
-	dataBuff := tcpBuffer[cim.IMHeaderLen:header.Length] // body=len-headLen
-	return header, dataBuff, nil
-}
-
 func heartBeat() {
 	for {
 		<-heartBeatTicker.C
 		_ = send(uint16(cim.CIMCmdID_kCIM_CID_LOGIN_HEARTBEAT), &cim.CIMHeartBeat{})
-		header, _, err := read()
+	}
+}
+
+func read() {
+	for {
+		buffLen, err := tcpConn.Read(tcpBuffer)
 		if err != nil {
 			logger.Sugar.Error("peer closed connection or network is unavailable", err.Error())
-			os.Exit(0)
-		} else if header.CommandId == uint16(cim.CIMCmdID_kCIM_CID_LOGIN_HEARTBEAT) {
-			// check heart beat
+			return
+		}
+		header := &cim.ImHeader{}
+		header.ReadHeader(tcpBuffer, buffLen)
+
+		if header.CommandId == uint16(cim.CIMCmdID_kCIM_CID_LOGIN_HEARTBEAT) {
+			continue
+		}
+
+		dataBuff := tcpBuffer[cim.IMHeaderLen:header.Length] // body=len-headLen
+		switch header.CommandId {
+		case uint16(cim.CIMCmdID_kCIM_CID_LIST_RECENT_CONTACT_SESSION_RSP):
+			onHandleRecentSessionList(dataBuff)
+			break
+		default:
+			logger.Sugar.Error("unknown command:%d", header.CommandId)
+			break
 		}
 	}
 }
 
-func showRecentSessionList() {
+
+func reqRecentSessionList() {
 	req := &cim.CIMRecentContactSessionReq{
 		UserId:           KUserId,
 		LatestUpdateTime: 0,
@@ -150,34 +163,26 @@ func showRecentSessionList() {
 		logger.Sugar.Error(err.Error())
 		return
 	}
+}
 
-	header, dataBuff, err := read()
-	if err != nil {
+func onHandleRecentSessionList(dataBuff []byte) {
+	rsp := &cim.CIMRecentContactSessionRsp{}
+	if err := proto.Unmarshal(dataBuff, rsp); err != nil {
 		logger.Sugar.Error(err.Error())
 		return
-	}
-
-	if header.CommandId == uint16(cim.CIMCmdID_kCIM_CID_LIST_RECENT_CONTACT_SESSION_RSP) {
-		rsp := &cim.CIMRecentContactSessionRsp{}
-		if err = proto.Unmarshal(dataBuff, rsp); err != nil {
-			logger.Sugar.Error(err.Error())
-			return
-		} else {
-			logger.Sugar.Infof("unread_cnt:%d", rsp.UnreadCounts)
-			for i := range rsp.ContactSessionList {
-				item := rsp.ContactSessionList[i]
-
-				sessionText := ""
-				if item.SessionType == cim.CIMSessionType_kCIM_SESSION_TYPE_SINGLE {
-					sessionText = fmt.Sprintf("%d", item.SessionId)
-				} else {
-					sessionText = "未知会话类型"
-				}
-				tm := time.Unix(int64(item.MsgTimeStamp), 0)
-				logger.Sugar.Infof("[%s][%d:%s][%s]", sessionText, item.MsgFromUserId, string(item.MsgData), tm.Format("2006-01-01 15:04:05 PM"))
-			}
-		}
 	} else {
-		logger.Sugar.Error("unknown command:%d", header.CommandId)
+		logger.Sugar.Infof("unread_cnt:%d", rsp.UnreadCounts)
+		for i := range rsp.ContactSessionList {
+			item := rsp.ContactSessionList[i]
+
+			sessionText := ""
+			if item.SessionType == cim.CIMSessionType_kCIM_SESSION_TYPE_SINGLE {
+				sessionText = fmt.Sprintf("%d", item.SessionId)
+			} else {
+				sessionText = "未知会话类型"
+			}
+			tm := time.Unix(int64(item.MsgTimeStamp), 0)
+			logger.Sugar.Infof("[%s][%d:%s][%s]", sessionText, item.MsgFromUserId, string(item.MsgData), tm.Format("2006-01-01 15:04:05 PM"))
+		}
 	}
 }
