a9f0455402cf67b8dfa3acf857cc93645ca2329d
xuyc <xmcy0011@sina.com>
2019-12-02 20:40:33 +0800

    修正第一次运行时，会重复创建数据库表导致报错的问题

--- client/cc_flutter_app/lib/imsdk/core/dao/sql_manager.dart
+++ client/cc_flutter_app/lib/imsdk/core/dao/sql_manager.dart
@@ -1,3 +1,4 @@
+import 'package:cc_flutter_app/imsdk/core/dao/session_db_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 import 'package:path/path.dart';
@@ -10,28 +11,35 @@ class SQLManager {
   static const _NAME = "coffeechat.db";
 
   static Database _database;
+  static var _isInit;
 
   /// 初始化
   static init() async {
+    _isInit = true;
     var databasesPath = await getDatabasesPath();
 
     String path = join(databasesPath, _NAME);
 
-    _database = await openDatabase(path,
-        version: _VERSION, onCreate: (Database db, int version) async {});
+    _database = await openDatabase(path, version: _VERSION, onCreate: (Database db, int version) async {});
+
+    // 初始化本地Session表
+    SessionDbProvider session = new SessionDbProvider();
+    var exist = await isTableExits(session.tableName());
+    if (!exist) {
+      await _database.execute(session.createTableString());
+    }
   }
 
   /// 判断表是否存在
   static isTableExits(String tableName) async {
     await getCurrentDatabase();
-    var res = await _database.rawQuery(
-        "select * from Sqlite_master where type = 'table' and name = '$tableName'");
+    var res = await _database.rawQuery("select * from Sqlite_master where type = 'table' and name = '$tableName'");
     return res != null && res.length > 0;
   }
 
   /// 获取当前数据库对象
   static Future<Database> getCurrentDatabase() async {
-    if (_database == null) {
+    if (_isInit == false) {
       await init();
     }
     return _database;
