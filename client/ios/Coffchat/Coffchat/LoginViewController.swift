2f759a68267669d432e21c817a1f02af71b394e0
xuyc <xmcy0011@sina.com>
2020-03-11 13:54:30 +0800

    提交ios工程，使用swift+uikit

---
+++ client/ios/Coffchat/Coffchat/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  Coffchat
+//
+//  Created by xuyingchun on 2020/3/10.
+//  Copyright © 2020 Xuyingchun Inc. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+// 请定义请求参数
+struct Login: Encodable {
+    let userName: String
+    let pwd: String
+}
+
+class LoginViewController: UIViewController {
+    var client = CIMClient()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        // 使用Alamofire发送HTTP请求
+//        let req = Login(userName: "10091009", pwd: "12345")
+//        AF.request("www.baidu.com", method: .post, parameters: req, encoder: JSONParameterEncoder.default).response { res in
+//            debugPrint(res)
+//        }
+    }
+ 
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    @IBAction func onLoginBtnClick(_ sender: Any) {
+        client.connect(ip: "10.0.106.117", port: 8000)
+    }
+    
+    @IBAction func onCancelBtnClick(_ sender: Any) {
+        client.close()
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ client/ios/Coffchat/Coffchat/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Coffchat
+//
+//  Created by xuyingchun on 2020/3/7.
+//  Copyright © 2020 Xuyingchun Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let imageView = UIImageView()
+//        let image = UIImage(named: "lanuch")
+//        imageView.image = image
+//
+//        self.view.addSubview(imageView)
+    }
+    
+    @IBAction func onLoginBtnClick(_ sender: Any) {
+        let loginView = LoginViewController()
+        self.present(loginView, animated: true, completion: nil)
+    }
+}
+
