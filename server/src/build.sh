73a10ec6efe18251e8a0c6c5271d0a120717d5ab
xuyc <xmcy0011@sina.com>
2020-04-14 12:47:44 +0800

    print build progress

--- server/src/build.sh
+++ server/src/build.sh
@@ -6,12 +6,14 @@ CUR_DIR=$(pwd)
 
 # 编译以守护进程启动工具
 build_daemon(){
+  echo "build daemon..."
   cd "$CUR_DIR" || exit
   go build -o app/daemon/daemon app/daemon/main.go
 }
 
 # 编译网关
 build_gate(){
+  echo "build gate..."
   cd "$CUR_DIR" || exit
   go build -o ${TARGET_DIR}/im_gate/im_gate app/im_gate/gate.go
   cp app/im_gate/gate-example.toml ${TARGET_DIR}/im_gate/im_gate.toml
@@ -20,14 +22,16 @@ build_gate(){
 
 # 编译logic
 build_logic(){
-   cd "$CUR_DIR" || exit
-   go build -o ${TARGET_DIR}/im_logic/im_logic app/im_logic/logic.go
-   cp app/im_logic/logic-example.toml ${TARGET_DIR}/im_logic/im_logic.toml
-   cp app/daemon/daemon ${TARGET_DIR}/im_logic/daemon
+  echo "build logic..."
+  cd "$CUR_DIR" || exit
+  go build -o ${TARGET_DIR}/im_logic/im_logic app/im_logic/logic.go
+  cp app/im_logic/logic-example.toml ${TARGET_DIR}/im_logic/im_logic.toml
+  cp app/daemon/daemon ${TARGET_DIR}/im_logic/daemon
 }
 
 # 编译http_server
 build_http(){
+  echo "build http..."
   cd "$CUR_DIR" || exit
   go build -o ${TARGET_DIR}/im_http/im_http app/im_http/http.go
   cp app/im_http/http-example.toml ${TARGET_DIR}/im_http/im_http.toml
