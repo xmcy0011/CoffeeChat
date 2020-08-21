339addbc466fb80cf32cd916eb22e0ac29a268a0
许迎春 <yingchun.xu@china.zhaogang.com>
2020-08-21 16:12:03 +0800

    change setup icon

--- client/win/setup/Niuniu_NSIS_SetupSkin/build-setup.bat
+++ client/win/setup/Niuniu_NSIS_SetupSkin/build-setup.bat
@@ -1,9 +1,9 @@
 @call makeapp.bat
 
-@call makeskinzip.bat nim
+@call makeskinzip.bat cim
 
-".\NSIS\makensis.exe" ".\SetupScripts\nim\nim_setup.nsi"
+".\NSIS\makensis.exe" ".\SetupScripts\cim\cim_setup.nsi"
 
 @rem 如果要调试错误，请使用下面的脚本，这样会打开编译界面（命令行界面中文会显示成?号）
-@rem ".\NSIS\makensisw.exe" ".\SetupScripts\nim\nim_setup.nsi"
+@rem ".\NSIS\makensisw.exe" ".\SetupScripts\cim\cim_setup.nsi"
 @pause
