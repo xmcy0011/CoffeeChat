2b7fe53ce371079cbad0af8a39093bdb6429f670
xmcy0011 <xmcy0011@sina.com>
2021-06-26 14:18:38 +0800

    init mac client dir

---
+++ client/mac/coffeechat/coffeechat/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  coffeechat
+//
+//  Created by fei.xu on 2021/6/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
