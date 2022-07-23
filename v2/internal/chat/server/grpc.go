1f9dcea3f5935ca20a8420c355c5cdd8ad7d7abb
xmcy0011 <xmcy0011@sina.com>
2022-07-23 16:42:01 +0800

    feat add logger meddleware

--- v2/internal/chat/server/grpc.go
+++ v2/internal/chat/server/grpc.go
@@ -5,6 +5,7 @@ import (
 	"CoffeeChat/internal/chat/conf"
 	"CoffeeChat/internal/chat/service"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 )
@@ -14,6 +15,7 @@ func NewGRPCServer(c *conf.Server, greeter *service.ChatService, logger log.Logg
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
+			logging.Server(logger),
 		),
 	}
 	if c.Grpc.Network != "" {
--- v2/internal/chat/server/http.go
+++ v2/internal/chat/server/http.go
@@ -5,6 +5,7 @@ import (
 	"CoffeeChat/internal/chat/conf"
 	"CoffeeChat/internal/chat/service"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
@@ -14,6 +15,7 @@ func NewHTTPServer(c *conf.Server, chatService *service.ChatService, logger log.
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			logging.Server(logger),
 		),
 	}
 	if c.Http.Network != "" {
