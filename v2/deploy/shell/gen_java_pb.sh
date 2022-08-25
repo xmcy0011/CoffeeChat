7e03962249366d4f7a8cea8f929438109e3ad6a1
xmcy0011 <xmcy0011@sina.com>
2022-08-25 11:17:44 +0800

    feat gen java pb shell

---
+++ v2/deploy/shell/gen_java_pb.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CUR_DIR=`cd ../../ && pwd`
+API_USER_DIR=${CUR_DIR}/api/apiuser/api/user/v1
+THIRD_PARTY_DIR=${CUR_DIR}/third_party
+
+PROTOC_DIRS="
+${API_USER_DIR}
+"
+
+echo "generate start..."
+for dir in ${PROTOC_DIRS}; do
+    for file in ${dir}/*.proto; do
+        # go
+        # protoc --proto_path=${dir} --proto_path=${CUR_DIR} --proto_path=${THIRD_PARTY_DIR} --go-grpc_out=${CUR_DIR} --go_out=${CUR_DIR} ${file}
+        # java
+        protoc --proto_path=${dir} --proto_path=${CUR_DIR} --proto_path=${THIRD_PARTY_DIR} --java_out=${CUR_DIR} ${file}
+        echo "gen ${file} ok"
+    done
+done
+echo "generate end!"
